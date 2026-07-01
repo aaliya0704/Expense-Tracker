a8ff601f2398dbfddb4f3c8d027bed24a86e5035
Aaliya Khan <aaliya.it2025@gmail.com>
2026-07-01 14:27:31 +0530

    Calculate total spending
    Added functionality to calculate the total spending by iterating through the list of expenses and summing the amount of each expense. Displayed the final total to provide users with an overview of their spending.

--- expense_tracker.py
+++ expense_tracker.py
@@ -12,10 +12,19 @@ def create_expense():
 
 
 def display_expense(expenses):
-    for expense in expenses:
+    for expense in expenses:  # for each of the expenses in the list print their Title, Amount, and Category using their respective keys
         print("Title    : ", expense["Title"])
         print("Amount   : ", expense["Amount"])
         print("Category : ", expense["Category"])
+
+
+def calculate_total(expenses):  # passing the entire list of expenses
+    total = 0  # initializing the accumulator variable total to 0. Think of it like a calculator that shows 0 before you start adding values in it. This is important because if you don't initialize it, the program won't know what to add to and will throw an error.
+    for expense in expenses:
+        total += expense[
+            "Amount"
+        ]  # fetches the amount from that expense dictionary -> adds that to the total (accumulator).
+    print("total spendings: ", total)
 
 
 def main():
@@ -36,7 +45,11 @@ def main():
     expenses.append(expense)
     print("Your expenses are as shown below: ")
 
+    # calling the display_expanse function to diplay each expense of the list
     display_expense(expenses)
+
+    # calling the calculate_taotal() function to calculate the total spendings
+    calculate_total(expenses)
 
 
 if __name__ == "__main__":
